ae9d8298e3f565d326bccf3c1b1a8e7f72c0005e
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-08-21 14:04:12 -0300

    remove next image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['client-project-4l-production.up.railway.app', 'localhost'],
+    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
--- src/components/atoms/gallery/Gallery.tsx
+++ src/components/atoms/gallery/Gallery.tsx
@@ -1,5 +1,4 @@
 import serverConfig from "@src/common/config/serverConfig/ServerConfig";
-import Image from "next/image";
 
 export default function Gallery({ images }: { images: string[] }) {
     if (images.length > 5)
@@ -8,7 +7,7 @@ export default function Gallery({ images }: { images: string[] }) {
                 <div className="grid-cols-3 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
                     {images.map((imageUrl, index) => (
                         <div key={index} className={index === 1 ? "w-full col-span-2 row-span-2 rounded-md" : "w-full rounded-md"}>
-                            <Image src={serverConfig.endpoint.path.file + imageUrl} alt={`image-${index}`} />
+                            <img src={serverConfig.endpoint.path.file + imageUrl} alt={`image-${index}`} />
                         </div>
                     ))}
                 </div>
@@ -19,7 +18,7 @@ export default function Gallery({ images }: { images: string[] }) {
             <div>
                 {images.map((imageUrl, index) => (
                     <div key={index} className={"w-full h-full"}>
-                        <Image src={serverConfig.endpoint.path.file + imageUrl} alt={`image-${index}`} />
+                        <img src={serverConfig.endpoint.path.file + imageUrl} alt={`image-${index}`} />
                     </div>
                 ))}
             </div>
