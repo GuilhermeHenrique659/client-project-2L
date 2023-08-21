02847d3853148d33ec3b2aa78b8457d3c9868394
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-08-21 11:32:16 -0300

    fix register screen

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -34,7 +34,7 @@ export default function Register() {
 
     return (
         <div className="flex h-screen justify-around items-center">
-            <div className="flex p-10 flex-col items-center shadow-lg rounded-lg justify-evenly content-center bg-cnt-dark lg:w-1/3 lg:h-2/4 sm:w-full sm:h-full">
+            <div className="flex p-10 flex-col items-center shadow-lg rounded-lg justify-evenly content-center bg-cnt-dark lg:w-1/3 lg:h-3/4 sm:w-full sm:h-full">
                 <FontAwesomeIcon icon={faTriangleExclamation}></FontAwesomeIcon>
                 <p className="text-sm p-2 m-2">Lembrando que este é um experimento, por favor, evite usar seu e-mail real ou senhas de outros sites. Seu e-mail não precisa ser válido, e a senha não precisa ser complexa.</p>
                 <h1 className="text-lg">Cadastro</h1>
