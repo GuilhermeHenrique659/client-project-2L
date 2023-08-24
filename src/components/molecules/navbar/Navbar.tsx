053eafbc8b80519cc7c26ac413d31fc05fa81890
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-08-24 14:44:50 -0300

    fix navbar erro

--- src/components/molecules/navbar/Navbar.tsx
+++ src/components/molecules/navbar/Navbar.tsx
@@ -5,24 +5,32 @@ import Button from "@src/components/atoms/button/Button";
 import UserNavBar from "@src/components/atoms/userNavbar/UserNavBar";
 import { CreateUserResponse } from "@src/repository/user/types/CreateUserResponse"
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 export default function Navbar() {
     const data = LocalStorageHelpers.get<CreateUserResponse>('user');
     const router = useRouter();
 
-    return (
-        <div className="bg-cnt-dark w-full h-fit p-5 flex items-center justify-between max-md:items-start max-md:flex-col md:flex-row">
-            <div className="">
-                <Button className="bg-cnt-dark shadow-none text-neutral-50 flex items-center" onClick={() => router.push('/')}>
-                <div className="rounded-md w-14 h-14 bg-slate-400" id="logo">
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    if (isClient)
+        return (
+            <div className="bg-cnt-dark w-full h-fit p-5 flex items-center justify-between max-md:items-start max-md:flex-col md:flex-row">
+                <div className="">
+                    <Button className="bg-cnt-dark shadow-none text-neutral-50 flex items-center" onClick={() => router.push('/')} >
+                        <div className="rounded-md w-14 h-14 bg-slate-400" id="logo">
+                        </div>
+                        <h3 className="ml-5 text-slate-50">AppName</h3>
+                    </Button>
                 </div>
-                    <h3 className="ml-5 text-slate-50">AppName</h3>
-                </Button>
+                <div className="max-md:mt-8 max-md:border-t md:pt-2">
+                    {data && <UserNavBar data={data}></UserNavBar>}
+                </div>
             </div>
-            <div className="max-md:mt-8 max-md:border-t md:pt-2">
-                {data && <UserNavBar data={data}></UserNavBar>}
-            </div>
-        </div>
-    )
+        )
 }
