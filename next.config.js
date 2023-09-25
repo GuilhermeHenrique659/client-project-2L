63c82d2d2627e7410d3ea1a72e972f9b8e604f43
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-09-25 11:28:21 -0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['client-project-4l-production.up.railway.app', 'localhost'],
+        domains: ['client-project-4l-production.up.railway.app', 'https://project-4l.onrender.com', 'localhost'],
     },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
         // your project has type errors.
         // !! WARN !!
-        ignoreBuildErrors: true,
+        ignoreBuildErrors: false,
     },
     eslint: {
         ignoreDuringBuilds: true,
--- src/components/organism/posts/PostsCommunity.tsx
+++ src/components/organism/posts/PostsCommunity.tsx
@@ -13,7 +13,7 @@ interface IPostProps {
     setPosts: Dispatch<SetStateAction<Post[]>>
 }
 
-export default function PostsCommuntiy({ posts, setPosts, communityId }: IPostProps){
+export default function PostsCommuntiy({ posts, setPosts, communityId }: IPostProps) {
     const [page, setPage] = useState<number>(0);
     const [loading, setLoading] = useState(true);
     const [emptyPosts, setEmptyPosts] = useState(false);
@@ -22,7 +22,7 @@ export default function PostsCommuntiy({ posts, setPosts, communityId }: IPostPr
         setPage((currentPage) => currentPage + 1);
         setLoading(true);
         const morePosts = await postSocketRepository.communityPosts(page + 1, communityId);
-        if(morePosts && morePosts.length > 0){
+        if (morePosts && morePosts.length > 0) {
             setPosts((currentPost) => [...currentPost, ...morePosts])
         } else {
             setEmptyPosts(true);
@@ -39,16 +39,16 @@ export default function PostsCommuntiy({ posts, setPosts, communityId }: IPostPr
 
     useEffect(() => {
         postSocketRepository.communityPosts(0, communityId).then((value) => {
-            if(value){
+            if (value) {
                 setPosts(value);
                 setLoading(false);
             }
         });
     }, [])
-    
+
     return (
         <div className="flex flex-col-reverse w-11/12 max-md:w-full items-center justify-around z-0">
-            {posts && posts.map((post) => <PostComponent key={post.id} post={post}></PostComponent>) }
+            {posts && posts.map((post) => <PostComponent key={post.id} post={post} setPosts={setPosts}></PostComponent>)}
             {loading && <Loading></Loading>}
             <Button onClick={handleLoadingMore} disabled={emptyPosts} className="w-64 h-8 items-center">Carregar mais</Button>
         </div>
